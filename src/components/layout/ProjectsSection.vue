272993d0014607b4d2d057e3abcf88a2346ee2b2
zeeshan-ahmad-dev <za5743759@gmail.com>
2024-12-13 21:08:27 +0500

    filepath

--- src/components/layout/ProjectsSection.vue
+++ src/components/layout/ProjectsSection.vue
@@ -18,7 +18,7 @@
           class="relative overflow-hidden transition-transform duration-300 border-2 border-yellow-300 rounded-lg hover:scale-105 hover:shadow-lg"
         >
           <img
-            src="/assets/gemini1.png"
+            src="../../assets/gemini1.png"
             alt="Gemini Project"
             class="w-full h-auto"
           />
@@ -33,7 +33,7 @@
           class="relative overflow-hidden transition-transform duration-300 border-2 border-yellow-300 rounded-lg hover:scale-105 hover:shadow-lg"
         >
           <img
-            src="/assets/TubeWizard.png"
+            src="../../assets/TubeWizard.png"
             alt="Tube Wizard"
             class="w-full h-auto"
           />
@@ -48,7 +48,7 @@
           class="relative overflow-hidden transition-transform duration-300 border-2 border-yellow-300 rounded-lg hover:scale-105 hover:shadow-lg"
         >
           <img
-            src="/assets/lugx.png"
+            src="../../assets/lugx.png"
             alt="Lugx Project"
             class="w-full h-auto"
           />
@@ -63,7 +63,7 @@
           class="relative overflow-hidden transition-transform duration-300 border-2 border-yellow-300 rounded-lg hover:scale-105 hover:shadow-lg"
         >
           <img
-            src="/assets/dashboard.png"
+            src="../../assets/dashboard.png"
             alt="Dashboard Project"
             class="w-full h-auto"
           />
@@ -79,7 +79,7 @@
       <div class="flex justify-center w-full">
         <a
           class="bg-[#f9b703] text-black font-semibold hover:shadow-lg transition-all border-none px-5 py-2 text-sm sm:text-base sm:px-6 sm:py-3 my-4 gap-2 rounded-full hover:bg-yellow-400"
-          href="/assets/Zeeshan Ahmad (1).pdf.pdf"
+          href="../../assets/Zeeshan Ahmad (1).pdf.pdf"
           download
           aria-label="Download my portfolio as PDF"
         >
